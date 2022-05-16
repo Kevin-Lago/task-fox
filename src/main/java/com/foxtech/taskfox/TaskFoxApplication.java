0b39857fcef35afd8c734dce666744eaa99df6f4
Kevin Lago <kevinthelago@gmail.com>
2022-05-16 16:56:23 -0400

    Initial Commit

---
+++ src/main/java/com/foxtech/taskfox/TaskFoxApplication.java
@@ -0,0 +1,13 @@
+package com.foxtech.taskfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFoxApplication.class, args);
+	}
+
+}
